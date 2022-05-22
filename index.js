6b6e6b72cbc4d1e5fc9584aeed3b407971e75bd8
nahidsultan19 <nahidsultan38@gmail.com>
2022-05-22 10:11:46 +0600

    added mongodb connection

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
+app.use(cors())
+app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tvlsx.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const partsCollection = client.db("partsManufacturer").collection("parts");
+        console.log('db connected')
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
